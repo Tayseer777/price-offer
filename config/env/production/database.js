49a90ce0f6090db07c1be55a73d15f7f5a0a27ae
tayseer1 <tayseer@vuedale.com>
2023-06-05 15:11:34 +0300

    asd

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,7 +1,7 @@
 // path: ./config/env/production/database.js
 
 const { parse } = require("pg-connection-string");
-
+//
 module.exports = ({ env }) => {
   const { host, port, database, user, password } = parse(env("DATABASE_URL"));
   
