bd88c6223418c2287b0f48003ffd0da7c24e053c
tayseer1 <tayseer@vuedale.com>
2023-06-06 14:22:36 +0300

    asd

--- config/plugins.js
+++ config/plugins.js
@@ -4,5 +4,37 @@ module.exports = {
       enabled: true,
       resolve: './src/plugins/link-button'
     },
+    'preview-button': {
+      config: {
+        contentTypes: [
+          {
+            uid: 'api::offer.offer',
+            draft: {
+              url: 'http://localhost:1337/api/offers',
+              query: {
+                type: 'offer',
+                id: '{id}',
+              },
+            },
+            published: {
+              url: 'http://offers.vuedale.com/offer/{id}',
+            },
+          },
+          // {
+          //   uid: 'api::client.client',
+          //   draft: {
+          //     url: 'http://localhost:1337/api/clients',
+          //     query: {
+          //       type: 'client',
+          //       id: '{id}',
+          //     },
+          //   },
+          //   published: {
+          //     url: 'http://offers.vuedale.com/client/{slug}',
+          //   },
+          // },
+        ],
+      },
+    },
     // ...
   }
--- src/plugins/link-button/admin/src/pages/HomePage/index.js
+++ src/plugins/link-button/admin/src/pages/HomePage/index.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import pluginId from '../../pluginId';
+import './HomePage.css'; // Import the CSS file for styling
 
 const HomePage = () => {
   return (
-    <div>
-      <h1>{pluginId}&apos;s HomePage</h1>
-      <p>Happy coding</p>
-      <Link to="/your-link-path">
-        <button>Go to Link</button>
-      </Link>
+    <div className="home-page">
+      {/* <h1>{pluginId}&apos;s HomePage</h1>
+      <p>Happy coding</p> */}
+      <div className="center-align">
+        <Link to="/your-link-path" className="link-button">
+          Go to Link
+        </Link>
+      </div>
     </div>
   );
 };
